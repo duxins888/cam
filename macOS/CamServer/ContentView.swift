b8cc169b198222e3ef97037458b5533205d2971b
Xin Du <duxins888@gmail.com>
2023-09-06 17:22:58 +0900

    Initial commit

---
+++ macOS/CamServer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  CamServer
+//
+//  Created by Xin Du on 2023/09/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOS/CamServer/MyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyApp.swift
+//  CamServer
+//
+//  Created by Xin Du on 2023/09/06.
+//
+
+import SwiftUI
+
+@main
+struct MyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
